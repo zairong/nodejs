4659288b499809ac8fc879a4d41339831519cab6
zairong <justiceman3211@gmail.com>
2025-09-05 15:08:24 +0800

    更新資料庫配置，將連接資訊改為使用環境變數以提高安全性和靈活性。

--- express-app/config/database.js
+++ express-app/config/database.js
@@ -1,10 +1,10 @@
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-  host: '3939',
-  user: 'root',
-  password: '1234',
-  database: 'express',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   connectionLimit: 5
 });
 
