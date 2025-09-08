01f02862c8fc50d05c91acb4e56c32518b8f905a
zairong <justiceman3211@gmail.com>
2025-09-08 09:55:37 +0800

    更新資料庫配置，移除不必要的 SSL 參數設定，以簡化連接配置。

--- express-app/config/database.js
+++ express-app/config/database.js
@@ -2,7 +2,6 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ...(isExternal ? { ssl: { rejectUnauthorized: false } } : {})
 });
 
 module.exports = pool;
